5a847402b7ca031bab0133c0758d6143264de7fd
psndna88 <psndna88@gmail.com>
2013-08-07 15:23:55 +0530

    update v2.2 defconfigs and build script

--- build_kernel_P3100.sh
+++ build_kernel_P3100.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 export KERNELDIR=`readlink -f .`
-CROSS_COMPILE=/Working_Directory/android_prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
 
 if [ ! -f $KERNELDIR/.config ];
 then
-  make defconfig psn_p3100_v2.1_oc_defconfig
+  make defconfig psn_p3100_v2.2_oc_defconfig
 fi
 
 . $KERNELDIR/.config
@@ -20,6 +19,6 @@ rm $KERNELDIR/BUILT-P3100/lib/modules/*
 rm $KERNELDIR/BUILT-P3100/zImage
 
 find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT-P3100/lib/modules/ \;
-/Working_Directory/android_prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-strip --strip-unneeded $KERNELDIR/BUILT-P3100/lib/modules/*
+${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT-P3100/lib/modules/*
 cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT-P3100/
 
--- build_kernel_P311x.sh
+++ build_kernel_P311x.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 export KERNELDIR=`readlink -f .`
-CROSS_COMPILE=/Working_Directory/android_prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
 
 if [ ! -f $KERNELDIR/.config ];
 then
-  make defconfig psn_p311x_v2.1_oc_defconfig
+  make defconfig psn_p311x_v2.2_oc_defconfig
 fi
 
 . $KERNELDIR/.config
@@ -20,6 +19,6 @@ rm $KERNELDIR/BUILT-P311x/lib/modules/*
 rm $KERNELDIR/BUILT-P311x/zImage
 
 find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT-P311x/lib/modules/ \;
-/Working_Directory/android_prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-strip --strip-unneeded $KERNELDIR/BUILT-P311x/lib/modules/*
+${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT-P311x/lib/modules/*
 cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT-P311x/
 

62de91e9bf36fabdc8ace653f0dd127108ac1b55
psndna88 <psndna88@gmail.com>
2013-09-30 14:02:50 +0530

    Added v2.3.4 defconfigs and updated buid scripts

--- build_kernel_N7100.sh
+++ build_kernel_N7100.sh
@@ -7,7 +7,7 @@ export ARCH=arm
 
 if [ ! -f $KERNELDIR/.config ];
 then
-   make defconfig psn_n7100_v2.3.3d_defconfig
+   make defconfig psn_n7100_v2.3.4_defconfig
 fi
 
 . $KERNELDIR/.config
--- build_kernel_N7105.sh
+++ build_kernel_N7105.sh
@@ -7,7 +7,7 @@ export ARCH=arm
 
 if [ ! -f $KERNELDIR/.config ];
 then
-   make defconfig psn_n7105_v2.3.3d_defconfig
+   make defconfig psn_n7105_v2.3.4_defconfig
 fi
 
 . $KERNELDIR/.config

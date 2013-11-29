0af35c7af5ba91ced9b04cfeaaa3e097ac1e5e93
psndna88 <psndna88@gmail.com>
2013-11-29 08:58:55 +0530

    Added v2.5 Mali-r3p1 (N7100 & N7105) defconfigs and updated build scripts

---
+++ build_kernel_mali-r3p1_N7105.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+. ~/AGNi_stamp_CM.sh
+. ~/gcc_4.7.2_armv7l.sh
+
+export ARCH=arm
+
+if [ ! -f $KERNELDIR/.config ];
+then
+   make defconfig psn_n7105_mali-r3p1_v2.5_defconfig
+fi
+
+. $KERNELDIR/.config
+
+mv .git .git-halt
+
+cd $KERNELDIR/
+make -j2 || exit 1
+
+mkdir -p $KERNELDIR/BUILT_N7105_r3p1/lib/modules
+
+rm $KERNELDIR/BUILT_N7105_r3p1/lib/modules/*
+rm $KERNELDIR/BUILT_N7105_r3p1/zImage
+
+find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT_N7105_r3p1/lib/modules/ \;
+${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT_N7105_r3p1/lib/modules/*
+cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT_N7105_r3p1/
+
+mv .git-halt .git
+

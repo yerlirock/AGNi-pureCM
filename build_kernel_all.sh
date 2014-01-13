ae0db24fabcc1fbe3e832d75ace21f0f395a524f
psndna88 <psndna88@gmail.com>
2014-01-13 13:33:42 +0530

    Added defconfigs and build scripts for SCH-I605 (t0ltevzw)

--- build_kernel_all.sh
+++ build_kernel_all.sh
@@ -13,5 +13,11 @@ echo "Building N7105 CM-type.....";
 ./build_kernel_N7105.sh && sleep 20 && rm .config;
 
 echo "Building N7105 OMNI-type.....";
-./build_kernel_omni_N7105.sh;
+./build_kernel_omni_N7105.sh && sleep 20 && rm .config;
+
+echo "Building I605 CM-type.....";
+./build_kernel_I605.sh && sleep 20 && rm .config;
+
+echo "Building I605 OMNI-type.....";
+./build_kernel_omni_I605.sh;
 

b3aa1fa3265c1a6fba73e0922ae65364cea332fd
psndna88 <psndna88@gmail.com>
2014-03-17 20:38:53 +0530

    Added support for P5100 & P511x

---
+++ build_kernel_P511x.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+. ~/AGNi_stamp_CM.sh
+. ~/gcc_4.8.3_linaro_cortex-a9.sh
+
+mv .git .git-halt
+
+export ARCH=arm
+
+if [ ! -f $KERNELDIR/.config ];
+then
+  make defconfig psn_p511x_v3.6_oc_defconfig
+fi
+
+. $KERNELDIR/.config
+
+echo "BEGINING KERNEL COMPILATION .........."
+
+cd $KERNELDIR/
+make -j2 || exit 1
+
+mkdir -p $KERNELDIR/BUILT-P511x/lib/modules
+rm $KERNELDIR/BUILT-P511x/lib/modules/*
+rm $KERNELDIR/BUILT-P511x/zImage
+
+echo "BEGINING SGX540 PVR KM COMPILATION ..........."
+cd $KERNELDIR/pvr_source/eurasiacon/build/linux2/omap4430_android
+make clean
+make TARGET_PRODUCT="blaze_tablet" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.4.2 || exit
+make clean
+mv $KERNELDIR/pvr_source/eurasiacon/binary2_540_120_omap4430_android_release/target/*.ko $KERNELDIR/BUILT-P511x/lib/modules/
+rm -rf $KERNELDIR/pvr_source/eurasiacon/binary2_540_120_omap4430_android_release
+
+echo "PREPARING BUILT-P511x ..........."
+cd $KERNELDIR
+find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT-P511x/lib/modules/ \;
+${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT-P511x/lib/modules/*
+cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT-P511x/
+
+mv .git-halt .git
+
+echo "COMPILATION TASKS FOR CM P511x COMPLETE !!!!!!!!"
--- build_kernel_all.sh
+++ build_kernel_all.sh
@@ -7,4 +7,11 @@ echo "Building P3100 CM.....";
 ./build_kernel_P3100.sh && sleep 20 && rm .config;
 
 echo "Building P311x CM.....";
-./build_kernel_P311x.sh
+./build_kernel_P311x.sh && sleep 20 && rm .config;
+
+echo "Building P5100 CM.....";
+./build_kernel_P5100.sh && sleep 20 && rm .config;
+
+echo "Building P511x CM.....";
+./build_kernel_P511x.sh
+

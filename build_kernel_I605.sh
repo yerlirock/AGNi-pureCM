9ae94c67338944cf5d25c24707d9ebdad4f9fea8
psndna88 <psndna88@gmail.com>
2014-04-15 20:35:28 +0530

    Updated v2.7.6 defconfigs build scripts

--- build_kernel_I605.sh
+++ build_kernel_I605.sh
@@ -7,7 +7,7 @@ export ARCH=arm
 
 if [ ! -f $KERNELDIR/.config ];
 then
-   make defconfig psn_i605_v2.7.5_defconfig
+   make defconfig psn_i605_v2.7.6_defconfig
 fi
 
 . $KERNELDIR/.config
--- build_kernel_N7100.sh
+++ build_kernel_N7100.sh
@@ -7,7 +7,7 @@ export ARCH=arm
 
 if [ ! -f $KERNELDIR/.config ];
 then
-   make defconfig psn_n7100_v2.7.5_defconfig
+   make defconfig psn_n7100_v2.7.6_defconfig
 fi
 
 . $KERNELDIR/.config

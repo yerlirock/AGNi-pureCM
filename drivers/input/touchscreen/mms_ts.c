756b648cfd126f34a6a8e054e82d57cec51dda8b
psndna88 <psndna88@gmail.com>
2014-06-11 22:18:09 +0530

    Implement: touchwake KnockON (arter97)

--- drivers/input/touchscreen/mms_ts.c
+++ drivers/input/touchscreen/mms_ts.c
@@ -153,6 +153,10 @@ struct device *sec_touchscreen;
 static struct device *bus_dev;
 
 int touch_is_pressed = 0;
+static bool knockon_reset = false;
+#ifdef CONFIG_TOUCH_WAKE
+static bool mms_ts_suspended = false;
+#endif
 
 #if defined(CONFIG_TARGET_LOCALE_KOR)
 static int noise_mode_indicator;
@@ -806,7 +810,20 @@ static irqreturn_t mms_ts_interrupt(int irq, void *dev_id)
 	}
 
 #ifdef CONFIG_TOUCH_WAKE
+	if (mms_ts_suspended) {
+		if (knockon) {
+			if (touch_is_pressed == 0) {
+				if (knockon_reset) {
+					knockon_reset = false;
   touch_press();
+				} else {
+					knockon_reset = true;
+				}
+			}
+		} else {
+			touch_press();
+		}
+	}
 #endif
 
 #if TOUCH_BOOSTER
@@ -3142,6 +3159,8 @@ static void mms_ts_early_suspend(struct early_suspend *h)
 	struct mms_ts_info *info;
 	info = container_of(h, struct mms_ts_info, early_suspend);
 	mms_ts_suspend(&info->client->dev);
+#else
+	mms_ts_suspended = true;
 #endif
 
 }
@@ -3152,6 +3171,8 @@ static void mms_ts_late_resume(struct early_suspend *h)
 	struct mms_ts_info *info;
 	info = container_of(h, struct mms_ts_info, early_suspend);
 	mms_ts_resume(&info->client->dev);
+#else
+	mms_ts_suspended = false;
 #endif
 }
 #endif

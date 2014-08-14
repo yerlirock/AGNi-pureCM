e11702f3753ae1b65d3b3097393999fa73cb5210
psndna88 <psndna88@gmail.com>
2014-08-14 20:58:33 +0530

    Add build scripts for wolfson variants

---
+++ build_kernel_I605_wolf.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+. ~/AGNi_stamp_CM.sh
+. ~/gcc_4.9.1_linaro_cortex-a9.sh
+
+export ARCH=arm
+
+if [ ! -f $KERNELDIR/.config ];
+then
+   make defconfig psn_i605_v2.8.3_wolf_defconfig
+fi
+
+. $KERNELDIR/.config
+
+mv .git .git-halt
+
+cd $KERNELDIR/
+make -j2 || exit 1
+
+mkdir -p $KERNELDIR/BUILT_I605_wolf/lib/modules
+
+rm $KERNELDIR/BUILT_I605_wolf/lib/modules/*
+rm $KERNELDIR/BUILT_I605_wolf/zImage
+
+find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT_I605_wolf/lib/modules/ \;
+${CROSS_COMPILE}strip --strip-unneeded $KERNELDIR/BUILT_I605_wolf/lib/modules/*
+cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT_I605_wolf/
+
+mv .git-halt .git
--- build_kernel_all.sh
+++ build_kernel_all.sh
@@ -6,9 +6,17 @@ cd $KERNELDIR;
 echo "Building N7100 .....";
 ./build_kernel_N7100.sh && sleep 20 && rm .config;
 
+echo "Building N7100 wolf .....";
+./build_kernel_N7100_wolf.sh && sleep 20 && rm .config;
+
 echo "Building N7105 .....";
 ./build_kernel_N7105.sh && sleep 20 && rm .config;
 
-echo "Building I605 .....";
-./build_kernel_I605.sh;
+echo "Building N7105 wolf.....";
+./build_kernel_N7105_wolf.sh && sleep 20 && rm .config;
 
+echo "Building I605 .....";
+./build_kernel_I605.sh && sleep 20 && rm .config;
+
+echo "Building I605 wolf .....";
+./build_kernel_I605_wolf.sh;
